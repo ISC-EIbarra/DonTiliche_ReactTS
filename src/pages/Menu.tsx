8e2315baa49786ffd32ef82113b435367b923258
Edson <EdsonP4100@gmail.com>
2025-04-24 15:18:36 -0700

    menu

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,15 +1,17 @@
 export default function Menu() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="relative min-h-screen flex flex-col bg-[url(./Don_Tiliche_bg.png)] bg-cover bg-center bg-no-repeat">
+      <div className="absolute inset-0 bg-black opacity-70"></div>
+
       {/* Contenedor superior */}
       <div
         id="Menú"
-        className="flex flex-col lg:flex-row min-h-[100vh] bg-white"
+        className="relative flex flex-col lg:flex-row min-h-[100vh]"
       >
         <div className="flex-1 flex justify-center px-8 py-8 mt-4">
           <div className="max-w-screen-md">
-            <p className="flex flex-col gap-4">
-              <h1 className="text-[#CA9652] text-4xl md:text-5xl font-semibold text-center">
+            <p className="flex flex-col gap-4 text-white">
+              <h1 className="text-4xl md:text-5xl font-semibold text-center">
                 Nuestro Menú
               </h1>
               <span>
