0e8b6642fb7c1da7ff8702996d9f29bc73cf25d0
Edson <EdsonP4100@gmail.com>
2025-07-31 19:28:05 -0700

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import Footer from './components/Footer/Footer';
-import NavBar from './components/NavMenu/Navbar';
 import NavMenu from './components/NavMenu/NavMenu';
 import Brand_Values from './pages/Brand_Values';
 import History_Mission from './pages/History_Mission';
@@ -10,7 +9,6 @@ function App() {
   return (
     <>
       <NavMenu />
-      <NavBar />
       <Home />
       <History_Mission />
       <Brand_Values />
