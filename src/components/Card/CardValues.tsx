def57262a79b30413b103b4f3297981f14269f57
Edson <EdsonP4100@gmail.com>
2025-08-01 14:09:13 -0700

    fix issues

--- src/components/Card/CardValues.tsx
+++ src/components/Card/CardValues.tsx
@@ -12,7 +12,7 @@ export default function CardValues({ actionValue }: CardValuesProps) {
         <Button
           disabled
           isIconOnly
-          className="bg-[#CA9652] text-white text-2xl font-medium text-center rounded-full px-4 py-4"
+          className="bg-[#CA9652] text-white text-2xl font-medium text-center rounded-full px-6 py-2"
         >
           {actionValue.id}
         </Button>
